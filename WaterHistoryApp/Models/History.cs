129c111ded3c7ba75128962f6f2a427e4af9a3c1
Евгений <43938668+Evgen1337@users.noreply.github.com>
2018-10-17 00:11:10 +0300

    Update History.cs

--- WaterHistoryApp/Models/History.cs
+++ WaterHistoryApp/Models/History.cs
@@ -14,20 +14,20 @@ namespace WaterHistoryApp.Models
     /// </summary>
     public class History
     {
-        public virtual int HistoryId { get; set; }
-        public virtual string Season { get; set; }
-        public virtual string NumberEmployees { get; set; }
-        public virtual string DayWeek { get; set; }
-        public virtual string PreviouDay { get; set; }
-        public virtual string TimeDelivery { get; set; }
-        public virtual string NumberPumps { get; set; }
-        public virtual string NumberRepeir { get; set; }
-        public virtual string Vending { get; set; }
-        public virtual string WorkingHours { get; set; }
-        public virtual string UncertainReasons { get; set; }
-        public virtual string TemperatureConditions { get; set; }
-        public virtual int Quantity { get; set; }
-        public virtual DateTime DateTime { get; set; }
+        public int HistoryId { get; set; }
+        public string Season { get; set; }
+        public string NumberEmployees { get; set; }
+        public string DayWeek { get; set; }
+        public string PreviouDay { get; set; }
+        public string TimeDelivery { get; set; }
+        public string NumberPumps { get; set; }
+        public string NumberRepeir { get; set; }
+        public string Vending { get; set; }
+        public string WorkingHours { get; set; }
+        public string UncertainReasons { get; set; }
+        public string TemperatureConditions { get; set; }
+        public int Quantity { get; set; }
+        public DateTime DateTime { get; set; }
 
 
     }
